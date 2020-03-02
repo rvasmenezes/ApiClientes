abb18c2831236aa648a23cfc08c42f955ecf9d6d
rvasmenezes <61674079+rvasmenezes@users.noreply.github.com>
2020-03-02 00:18:58 -0300

    Add files via upload

---
+++ Infra/Config/ContextBase.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Config
+{
+    public class ContextBase : DbContext
+    {
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+     
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(StringConnectionConfig());
+            }
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+
+        private string StringConnectionConfig()
+        {
+            string strConf = "server=DESKTOP-S8VP4K4\\SQLEXPRESS;User Id=sa;password=123456;Persist Security Info=True;database=DDDTesteCore";
+            return strConf;
+        }
+    }
+}
